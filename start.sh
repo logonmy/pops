1b2b61d5525a74fe321d9e6dd12ebba72201194f
Shuge Lee <shuge.lee@gmail.com>
2014-08-05 18:55:15 +0800

    Fixed start.sh.

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 python pops.py \
-	# --processes `python -c "import multiprocessing; print multiprocessing.cpu_count()"` \
-	--processes 64 \
+	--processes `python -c "import multiprocessing; print multiprocessing.cpu_count()"` \
 	--error_log /var/log/pops.log \
 	--pid /var/run/pops.pid \
 	--mode slot \
 	--daemon
-
