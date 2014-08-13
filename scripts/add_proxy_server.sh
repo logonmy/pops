2c1f78a12bc238a21996204b82ce02edf8a89ba7
Shuge Lee <shuge.lee@gmail.com>
2014-08-13 11:19:40 +0800

    Improving add_proxy_server.sh.

--- scripts/add_proxy_server.sh
+++ scripts/add_proxy_server.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-slot_addr=$1
-node_addr=$2
+DEFAULT_ADMIN_AUTH='god:hidemyass'
 
-if [ "$slot_addr" = "" ] || [ "$node_addr" = "" ] ;then
-	echo "Usage $0: <slot_addr> <node_addr file or string>"
+slot_host_port=$1
+node_host_port=$2
+
+if [ "$ADMIN_AUTH" = "" ]; then
+  ADMIN_AUTH=$DEFAULT_ADMIN_AUTH
+fi
+
+if [ "$slot_host_port" = "" ] || [ "$node_host_port" = "" ] ;then
+	echo "Usage $0: <slot host_port> <node host_port file or string>"
 	exit 0
 fi
 
-if [ -f $node_addr ]; then
-    for i in `cat $node_addr |grep -v ^$|grep -v '#'`;do
-        curl --basic --user 'god:hidemyass' "http://$slot_addr/admin/node/add?addr=$i"
-    done
+if [ -f "$node_host_port" ]; then
+    host_addr_port_list=`cat "$node_host_port" |grep -v ^$ | sort | uniq | tr "\\n" "," | sed 's/\(.*\),/\1/'`
+    curl --user $ADMIN_AUTH "http://$slot_host_port/admin/node/add?addr=$host_addr_port_list"
 else
-    curl --basic --user 'god:hidemyass' "http://$slot_addr/admin/node/add?addr=$node_addr"
+    curl --user $ADMIN_AUTH "http://$slot_host_port/admin/node/add?addr=$node_host_port"
 fi
 
 exit 0
